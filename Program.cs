148a9671df845f2f18dc12953fa9b1fe3a85ae11
Ruben Villagrana <111696775+RubenVillagrana@users.noreply.github.com>
2023-02-08 19:40:18 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-
+    / *  Author: Ruben Villagrana
+    *   Course: COMP-003A
+    *   Purpose: Code for Lecture Activity 4
+    */  Reference: 
+        https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-
+        statements
+    */
+   namespace COMP003A.LectureActivity4
+   {
+   internal class Program
+   {
+   static void Main(string[] args)
+   {
+      
